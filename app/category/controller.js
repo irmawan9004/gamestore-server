4bf258a996fcd0d5b65b74571e433dbc78c08dcb
irmawan <irmawan9001@gmail.com>
2022-03-10 23:40:50 +0700

    add edit in category adminlte

--- app/category/controller.js
+++ app/category/controller.js
@@ -17,12 +17,34 @@ module.exports = {
       console.log(error);
     }
   },
+  viewEdit: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const category = await Category.findOne({ _id: id });
+
+      res.render("admin/category/edit", { category });
+    } catch (error) {
+      console.log(error);
+    }
+  },
 
   actionCreate: async (req, res) => {
     try {
       const { name } = req.body;
       let category = await Category({ name });
       await category.save();
+
+      res.redirect("/category");
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  actionEdit: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { name } = req.body;
+
+      const category = await Category.findOneAndUpdate({ _id: id }, { name });
 
       res.redirect("/category");
     } catch (error) {
