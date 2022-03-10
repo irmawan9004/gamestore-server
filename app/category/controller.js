4d908a34d4a06d1a5c12dbb1fd14daf78c2177f3
irmawan <irmawan9001@gmail.com>
2022-03-10 23:46:07 +0700

    add delete in category adminlte

--- app/category/controller.js
+++ app/category/controller.js
@@ -51,4 +51,14 @@ module.exports = {
       console.log(error);
     }
   },
+  actionDelete: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const category = await Category.findOneAndRemove({ _id: id });
+
+      res.redirect("/category");
+    } catch (error) {
+      console.log(error);
+    }
+  },
 };
