f5ab26390fa4eef815606f132066e234b2352b06
irmawan <irmawan9001@gmail.com>
2022-03-21 17:17:24 +0700

    delete payment adminlte

--- app/payment/controller.js
+++ app/payment/controller.js
@@ -66,17 +66,17 @@ module.exports = {
       res.redirect("/payment");
     }
   },
-  // actionDelete: async (req, res) => {
-  //   try {
-  //     const { id } = req.params;
-  //     await Nominal.findOneAndRemove({ _id: id });
-  //     req.flash("alertMessage", `Berhasil Menghapus Nominal`);
-  //     req.flash("alertStatus", "success");
-  //     res.redirect("/nominal");
-  //   } catch (error) {
-  //     req.flash("alertMessage", `${error.message}`);
-  //     req.flash("alertStatus", "danger");
-  //     res.redirect("/nominal");
-  //   }
-  // },
+  actionDelete: async (req, res) => {
+    try {
+      const { id } = req.params;
+      await Payment.findOneAndRemove({ _id: id });
+      req.flash("alertMessage", `Berhasil Menghapus Payment`);
+      req.flash("alertStatus", "success");
+      res.redirect("/payment");
+    } catch (error) {
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/payment");
+    }
+  },
 };
