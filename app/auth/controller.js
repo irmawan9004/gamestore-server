b6a43b8e8e1f19ae708b44cbe740d39df5391874
irmawan <irmawan9001@gmail.com>
2022-03-30 23:53:43 +0700

    sign in api

--- app/auth/controller.js
+++ app/auth/controller.js
@@ -2,6 +2,8 @@ const Player = require("../player/model");
 const path = require("path");
 const fs = require("fs");
 const config = require("../../config");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
   signUp: async (req, res, next) => {
@@ -61,5 +63,44 @@ module.exports = {
       }
       next();
     }
+  },
+  signIn: async (req, res, next) => {
+    const { email, password } = req.body;
+
+    Player.findOne({ email: email })
+      .then((player) => {
+        if (player) {
+          const checkPassword = bcrypt.compareSync(password, player.password);
+          if (checkPassword) {
+            const token = jwt.sign(
+              {
+                player: {
+                  id: player.id,
+                  username: player.username,
+                  email: player.email,
+                  name: player.name,
+                  phoneNumber: player.phoneNumber,
+                  avatar: player.avatar,
+                },
+              },
+              config.jwtKey
+            );
+
+            res.status(200).json({ data: token });
+          } else {
+            res
+              .status(403)
+              .json({ message: "Password yang anda masukkan salah" });
+          }
+        } else {
+          res.status(403).json({ message: "Email yang anda masukkan salah" });
+        }
+      })
+      .catch((err) => {
+        res
+          .status(500)
+          .json({ message: err.message || "Internal server error" });
+        next();
+      });
   },
 };
