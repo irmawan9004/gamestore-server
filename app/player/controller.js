573ceb8f2b354e829ef47080407403ae8d666627
irmawan <irmawan9001@gmail.com>
2022-03-31 23:50:49 +0700

    checkout api part 1

--- app/player/controller.js
+++ app/player/controller.js
@@ -1,6 +1,9 @@
 const Player = require("./model");
 const Voucher = require("../voucher/model");
 const Category = require("../category/model");
+const Payment = require("../payment/model");
+const Bank = require("../bank/model");
+const Nominal = require("../nominal/model");
 
 module.exports = {
   landingPage: async (req, res) => {
@@ -44,5 +47,31 @@ module.exports = {
         .status(500)
         .json({ message: error.message || "Terjadi kesalahan pada server" });
     }
+  },
+  checkout: async (req, res) => {
+    const { accountUser, name, nominal, voucher, payment, bank } = req.body;
+
+    const res_voucher = await Voucher.findOne({ _id: voucher })
+      .select("name category _id thumbnail user")
+      .populate("category")
+      .populate("user");
+
+    if (!res_voucher)
+      return res.status(404).json({ message: "voucher game tidak ditemukan" });
+
+    const res_nominal = await Nominal.findOne({ _id: nominal });
+
+    if (!res_nominal)
+      return res.status(404).json({ message: "nominal  tidak ditemukan" });
+
+    const res_payment = await Payment.findOne({ _id: payment });
+
+    if (!res_payment)
+      return res.status(404).json({ message: "payment  tidak ditemukan" });
+
+    const res_bank = await Bank.findOne({ _id: bank });
+
+    if (!res_bank)
+      return res.status(404).json({ message: "bank  tidak ditemukan" });
   },
 };
