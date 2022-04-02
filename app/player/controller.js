2907df21dfa1530f21592dbeb20bfa4fea40c06c
irmawan <irmawan9001@gmail.com>
2022-04-03 00:08:30 +0700

    get historyDetail api

--- app/player/controller.js
+++ app/player/controller.js
@@ -144,4 +144,21 @@ module.exports = {
         .json({ message: error.message || "Internal server Error" });
     }
   },
+
+  historyDetail: async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      const history = await Transaction.findOne({ _id: id });
+
+      if (!history)
+        return res.status(404).json({ message: "Transaction not found" });
+
+      res.status(200).json({ data: history });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: error.message || "Internal server Error" });
+    }
+  },
 };
--- app/player/router.js
+++ app/player/router.js
@@ -1,6 +1,13 @@
 var express = require("express");
 var router = express.Router();
-const { landingPage, detailPage, category, checkout } = require("./controller");
+const {
+  landingPage,
+  detailPage,
+  category,
+  checkout,
+  history,
+  historyDetail,
+} = require("./controller");
 const { isLoginPlayer } = require("../../middleware/auth");
 
 /* GET home page. */
@@ -8,5 +15,7 @@ router.get("/landingpage", landingPage);
 router.get("/:id/detail", detailPage);
 router.get("/category", category);
 router.post("/checkout", isLoginPlayer, checkout);
+router.get("/history", isLoginPlayer, history);
+router.get("/history/:id/detail", isLoginPlayer, historyDetail);
 
 module.exports = router;
